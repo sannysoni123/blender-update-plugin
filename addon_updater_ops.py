47be1c1c792a3e7aeb0e286c4393cd7613c814e0
sannysoni123 <sanny.soni@einfochips.com>
2021-01-21 18:09:08 +0530

    committed

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1354,7 +1354,7 @@ def register(bl_info):
 	updater.subfolder_path = ""
 
 	# used to check/compare versions
-	updater.current_version = bl_info["version"] + get_current_version
+	updater.current_version =  get_current_version
 	# Optional, to hard-set update frequency, use this here - however,
 	# this demo has this set via UI properties.
 	# updater.set_check_interval(
